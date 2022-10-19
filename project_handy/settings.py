6368b7d38c626fa99d7a7d84153eef39faed4cb4
Zuza Zaremska <zuza.zaremska@gmail.com>
2022-10-19 19:04:14 +0200

    Added static files. Set tracker_home page front-end.

--- project_handy/settings.py
+++ project_handy/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #my apps
     'app_tracker.apps.AppTrackerConfig',
+    'bootstrap5',
 
     
     #default apps
@@ -123,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
