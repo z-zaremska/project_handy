2beba6da926e433456c79a68d5ee8ea5b175db6c
Zuza Zaremska <zuza.zaremska@gmail.com>
2022-10-26 21:29:08 +0200

    Set front-end for login, logout, register, 404 and 500 pages. Log in, log out and register possible.

--- users/views.py
+++ users/views.py
@@ -4,4 +4,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     """New users registration"""
-    pass
+
+    if request.method != 'POST':
+        registration_form = UserCreationForm()
+        return render(request, 'registration/register.html', {'registration_form': registration_form})
+    
+    else:
+        registration_form = UserCreationForm(data=request.POST)
+        if registration_form.is_valid():
+            new_user = registration_form.save()
+            login(request, new_user)
+            return redirect('project_home')
