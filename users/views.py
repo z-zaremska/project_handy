8f56e1c9df332199a62c0f42ca9ed131087ba9d0
Zuza Zaremska <zuza.zaremska@gmail.com>
2022-10-27 21:44:30 +0200

    Navbar in base_project splited into two sides. Fix redirecting after logging in.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def register(request):
     """New users registration"""
@@ -15,3 +15,17 @@ def register(request):
             new_user = registration_form.save()
             login(request, new_user)
             return redirect('project_home')
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('project_home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
