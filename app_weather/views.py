c87a3e447df16356c43df88f9165a91f3dc738a5
“Zuza <zuza.zaremska@gmail.com>
2023-02-09 20:23:47 +0100

    Set template into 5 days forecast

--- app_weather/views.py
+++ app_weather/views.py
@@ -7,33 +7,22 @@ import json
 def weather_home(request):
 
     # TODO: maybe geolocation?
-    city = "Gdansk"
+    city = "Krakow"
 
     if request.method == "POST":
         city = request.POST['city'].title()
 
-
-    #"api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={config('WEATHER_API_KEY')}&units=metric&lang=en"
-    api_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid="
+    api_url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&cnt=5&appid={config("WEATHER_API_KEY")}&units=metric&lang=en'
     source = urllib.request.urlopen(api_url).read()
     weather_data = json.loads(source)
 
     context = {
-        "name": weather_data['name'],
-        "country_code": weather_data['sys']['country'],
-        "sunrise": weather_data['sys']['sunrise'],
-        "sunset": weather_data['sys']['sunset'],
-        "temp": weather_data['main']['temp'],
-        "temp_min": weather_data['main']['temp_min'],
-        "temp_max": weather_data['main']['temp_max'],
-        "pressure": weather_data['main']['pressure'],
-        "humidity": weather_data['main']['humidity'],
-        "main": weather_data['weather'][0]['main'],
-        "description": weather_data['weather'][0]['description'],
-        "visibility": weather_data['visibility'],
-        "wind_speed": weather_data['wind']['speed'],
-        "clouds": weather_data['clouds']['all'],
-        "dt": weather_data['dt'],
+        "name": weather_data['city']['name'],
+        "country_code": weather_data['city']['country'],
+        "sunrise": weather_data['city']['sunrise'],
+        "sunset": weather_data['city']['sunset'],
+        'today_forecast': weather_data['list'][0],
+        'next_days_forecast': weather_data['list'][1:],
     }
 
     return render(request, 'app_weather/weather_home.html', context)
